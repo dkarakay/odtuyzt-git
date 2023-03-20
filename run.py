ac31b92390afa182b81a9e4e50119d90f3e9e0de
Deniz Karakay <deniz@karakay.me>
2023-03-20 19:30:44 +0300

    Initial commit 🔥🚀𝗪

---
+++ run.py
@@ -0,0 +1,41 @@
+from core.main import auto_checker_capacity
+
+semester = '20222'  # 2022-2023 Spring
+department_code = '651'  # Electrical Electronics Engineering
+class_code = '242'  # Digital Circuits Laboratory
+section_no = 1  # Section 8
+
+driver_path = "/Users/sedna/local-dev/chromedriver"
+refresh_rate = 4  # Check each 4 seconds
+
+discord_webhook_url = 'https://discord.com/api/webhooks/xxxx'
+
+# Deprecated for now
+
+# auto_checker(
+#     class_code=class_code,
+#     department_code=department_code,
+#     discord_bot=False,
+#     discord_webhook_url=discord_webhook_url,
+#     driver_path=driver_path,
+#     refresh_rate=refresh_rate,
+#     section_no=section_no,
+#     semester=semester,
+#     telegram_bot=False,
+#     voice_feedback=True,
+# )
+
+# Check capacity increase
+auto_checker_capacity(
+    class_code=class_code,
+    department_code=department_code,
+    discord_bot=False,
+    discord_webhook_url=discord_webhook_url,
+    driver_path=driver_path,
+    refresh_rate=refresh_rate,
+    section_no=section_no,
+    semester=semester,
+    voice_feedback=True,
+    first_time=True,
+    # telegram_bot=False,
+)
